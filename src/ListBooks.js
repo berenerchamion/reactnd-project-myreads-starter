1d223e6144bd6bb951734ce8e4f29170398dfb7d
Beren Erchamion <beren@houseofbeor.net>
2018-02-13 14:19:11 -0500

    First draft of shelf is working with API calls.

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -11,6 +11,13 @@ class ListBooks extends Component {
 
   render(){
     const { books } = this.props;
+    let currentShelf
+    let wantToReadShelf
+    let readShelf
+
+    currentShelf = books.filter((books) => books.shelf === 'currentlyReading');
+    wantToReadShelf = books.filter((books) => books.shelf === 'wantToRead');
+    readShelf = books.filter((books) => books.shelf === 'read');
 
     return(
         <div className="list-books-content">
@@ -19,11 +26,12 @@ class ListBooks extends Component {
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  {books.map((books) => (
-                  <li>
+                  {currentShelf.map((book) => (
+                  <li key={book.id} className="bookListItem">
                     <div className="book">
                       <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                        <div className="book-cover"
+                          style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
                         <div className="book-shelf-changer">
                           <select>
                             <option value="none" disabled>Move to...</option>
@@ -34,13 +42,86 @@ class ListBooks extends Component {
                           </select>
                         </div>
                       </div>
-                      <div className="book-title">To Kill a Mockingbird</div>
-                      <div className="book-authors">Harper Lee</div>
+                      <div className="book-title">{book.title}</div>
+                      <div className="book-authors">
+                        {book.authors.map((author) => (
+                          <span key={author}>{ author } </span>
+                        ))}
+                      </div>
+                      <div className="book-title">{book.shelf}</div>
                     </div>
                   </li>
                   ))}
                 </ol>
               </div>
+
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Want to Read</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {wantToReadShelf.map((book) => (
+                    <li key={book.id} className="bookListItem">
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover"
+                            style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                          <div className="book-shelf-changer">
+                            <select>
+                              <option value="none" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">
+                          {book.authors.map((author) => (
+                            <span key={author}>{ author } </span>
+                          ))}
+                        </div>
+                        <div className="book-title">{book.shelf}</div>
+                      </div>
+                    </li>
+                    ))}
+                  </ol>
+                </div>
+              </div>
+
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Want to Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {readShelf.map((book) => (
+                      <li key={book.id} className="bookListItem">
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover"
+                              style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="none" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">
+                            {book.authors.map((author) => (
+                              <span key={author}>{ author } </span>
+                            ))}
+                          </div>
+                          <div className="book-title">{book.shelf}</div>
+                        </div>
+                      </li>
+                      ))}
+                    </ol>
+                  </div>
+                </div>
             </div>
           </div>
           <div className="open-search">
