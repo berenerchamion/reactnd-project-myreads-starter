13ff71562327185db6105327af1d5846ae8f8780
Beren Erchamion <beren@houseofbeor.net>
2018-02-20 18:20:45 -0500

    book component....grrrr.....

---
+++ src/Book.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class Book extends Component{
+  
+}
+
+export default Book
